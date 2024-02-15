5971432ad4763d01f63fd4b8c10d9fad9a8ef616
intellectdigits <115778808+intellectdigits@users.noreply.github.com>
2024-02-15 03:12:29 +0300

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -44,40 +44,8 @@ if(value.trim()==""){
 }
 
 }
-const handleCategory=(e)=>{
-  const value=e.target.value;
-const name =e.target.name;
-setData({...Data,[name]:value})
-if(value.trim()==""){
- setError({...error,category:"lastmame must not be empty"})
-}else{
-  setError({...error,category:""})
-}
 
-}
-const handleCourse=(e)=>{
-  const value=e.target.value;
-const name =e.target.name;
 
-setData({...Data,[name]:value})
-if(value.trim()==""){
- setError({...error,course:"password must not be empty"})
-}else{
-  setError({...error,course:""})
-}
-
-}
-const handleInstructor=(e)=>{
-  const value=e.target.value;
-const name =e.target.name;
-setData({...Data,[name]:value})
-if(value.trim()==""){
- setError({...error,instructor:"password must not be empty"})
-}else{
-  setError({...error,instructor:""})
-}
-
-}
 
 
 
@@ -117,40 +85,9 @@ startTime:Data.start_time,
     <input type="text" onChange={handleFullname} value={Data.fullname} name="fullname" className="bg-white-50 border border-green-500 text-green-900 placeholder-gray-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-green-100 dark:border-green-400" placeholder="fullname"/>
     {error.fullname==="fullname must not be empty"?<p className="mt-2 text-sm text-green-600 dark:text-green-500"><span className="font-medium">Alright!</span> Username available!</p>:""}
    </div>
-   <div className="mb-5">
-   <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-<select name="category" onChange={handleCategory} className="bg-gray-50 border border-green-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  <option selected>Select Category</option>
-  <option value="US">Software Development</option>
-  <option value="CA">Business</option>
-  <option value="FR">Accounting</option>
-  <option value="DE">Other</option>
-</select>
-    {error.lastname==="lastmame must not be empty"?<p className="mt-2 text-sm text-green-600 dark:text-green-500"><span className="font-medium">Alright!</span> Username available!</p>:""}
-   </div>
-   <div className="mb-5">
-   <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Course</label>
-<select name="course" onChange={handleCourse} className="bg-gray-50 border border-green-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  <option selected>Select Course</option>
-  <option value="US">Javascript</option>
-  <option value="CA">PHP</option>
-  <option value="FR">:Laravel</option>
-  <option value="DE">Other</option>
-</select>
-    {error.instructor==="lastmame must not be empty"?<p className="mt-2 text-sm text-green-600 dark:text-green-500"><span className="font-medium">Alright!</span> Username available!</p>:""}
-   </div>
-   <div className="mb-5">
-   <label for="countries" onChange={handleInstructor} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Instructor</label>
-<select name="countries" className="bg-gray-50 border border-green-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  <option selected>Choose a country</option>
-  <option value="US">Victor Great</option>
-  <option value="CA">Peter Ofem</option>
-  <option value="FR">Felix efe</option>
-  <option value="DE">Other</option>
-</select>
-    {error.instructor==="lastmame must not be empty"?<p className="mt-2 text-sm text-green-600 dark:text-green-500"><span className="font-medium">Alright!</span> Username available!</p>:""}
-   </div>
-  <div className="flex">
+  
+   
+
   <div className="mb-5">
     <label for="username-success" className="block py-1 mb-2 text-lg text-bold font-medium text-dark-700 dark:text-green-500">Start Date</label>
     <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
